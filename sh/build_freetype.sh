b7e0a50be89a870522b37c4a3d2bf66ba00d9105
mabangjin <mabangjin@outlook.com>
2020-09-22 16:51:11 +0800

    修改

--- sh/build_freetype.sh
+++ sh/build_freetype.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 git submodule update --init ../External/src/freetype
-cp -f -R ../External/src/freetype/include ../External/Windows/
 cp -f -R ../External/src/freetype/win32/freetype.lib ../External/Windows/lib
 
