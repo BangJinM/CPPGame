786ee030bbd8131b81773795ef56bc6e678d5c0d
mabangjin <mabangjin@outlook.com>
2020-11-16 09:22:48 +0800

    FileView

--- Editor/FileView.cpp
+++ Editor/FileView.cpp
@@ -4,26 +4,73 @@
 #include <QFileSystemModel>
 #include <QDir>
 
+
 FileView::FileView(QWidget *parent) : QDockWidget(parent),
                                       ui(new Ui::FileView)
 {
     ui->setupUi(this);
 
-    QString _logDir = "E:/games/fgcq/fgcq_tools";
-    QFileSystemModel *_fileModel = new QFileSystemModel();
-    _fileModel->setRootPath(QDir::currentPath()); //_logDir
+    _fileRootPath = "E:/games/fgcq/fgcq_tools";
+    _curPath = _fileRootPath;
 
-    ui->treeView->setModel(_fileModel);
-    ui->treeView->setRootIndex(_fileModel->index(_logDir));
-
-    //隐藏文件名后面的信息
-    for (int i = 1; i < _fileModel->columnCount(); i++)
-    {
-        ui->treeView->setColumnHidden(i, true);
-    }
+    init();
 }
 
 FileView::~FileView()
 {
+    delete _listModel;
+    delete _treeModel;
     delete ui;
+}
+
+void FileView::init(){
+    _treeModel = new QFileSystemModel();
+    _treeModel->setRootPath(QDir::currentPath()); //_logDir
+
+    ui->treeView->setModel(_treeModel);
+    ui->treeView->setRootIndex(_treeModel->index(_fileRootPath));
+
+    //隐藏文件名后面的信息
+    for (int i = 1; i < _treeModel->columnCount(); i++)
+    {
+        ui->treeView->setColumnHidden(i, true);
+    }
+
+    _listModel = new QFileSystemModel();
+    _listModel->setRootPath(QDir::currentPath());
+    ui->listView->setModel(_listModel);
+    ui->listView->setViewMode(QListView::IconMode); //ListMode
+    ui->listView->setRootIndex(_listModel->index(_curPath));
+}
+
+void FileView::setRootPath(QString path){
+    _fileRootPath = path;
+    setTreeDir(_fileRootPath);
+    setListDir(_fileRootPath);
+}
+
+void FileView::setTreeDir(QString treePath){
+    _treeModel->setRootPath(treePath);
+    ui->treeView->setRootIndex(_treeModel->index(treePath));
+}
+
+void FileView::setListDir(QString listPath){
+    _listModel->setRootPath(listPath);
+    ui->listView->setRootIndex(_listModel->index(listPath));
+}
+
+void FileView::on_treeView_doubleClicked(const QModelIndex &index)
+{
+    QFileInfo var = _listModel->fileInfo(index);
+    if(var.isDir())
+        setListDir(var.filePath());
+}
+
+void FileView::on_listView_doubleClicked(const QModelIndex &index)
+{
+    QFileInfo var = _listModel->fileInfo(index);
+    if(var.isDir())
+        setListDir(var.filePath());
+    if (var.isFile())
+        printf("dsadasdasdas");
 }
--- Editor/FileView.h
+++ Editor/FileView.h
@@ -2,6 +2,7 @@
 #define FILEVIEW_H
 
 #include <QDockWidget>
+#include <QFileSystemModel>
 
 namespace Ui {
 class FileView;
@@ -12,13 +13,26 @@ class FileView : public QDockWidget
     Q_OBJECT
 
 public:
+    void setRootPath(QString path);
     explicit FileView(QWidget *parent = nullptr);
     ~FileView();
 
+private slots:
+    void on_treeView_doubleClicked(const QModelIndex &index);
+
+    void on_listView_doubleClicked(const QModelIndex &index);
+private:
+
+    void init();
+    void setTreeDir(QString);
+    void setListDir(QString);
 private:
     Ui::FileView *ui;
 
-    QString curPath = "";
+    QFileSystemModel *_listModel;
+    QFileSystemModel *_treeModel;
+    QString _fileRootPath = "";
+    QString _curPath = "";
 };
 
 #endif // FILEVIEW_H
