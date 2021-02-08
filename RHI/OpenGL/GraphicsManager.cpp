daf1b4fc3f1692bd18d72eefb338d6d8c99497ae
mabangjin <mabangjin@outlook.com>
2021-02-08 16:11:49 +0800

    * 删除调试代码

--- RHI/OpenGL/GraphicsManager.cpp
+++ RHI/OpenGL/GraphicsManager.cpp
@@ -23,41 +23,6 @@
 #include "easylogging++.h"
 using namespace std;
 
-static GLenum glCheckError1(const char *file = __FILE__, int line = __LINE__)
-{
-	GLenum errorCode;
-	while ((errorCode = glGetError()) != GL_NO_ERROR)
-	{
-		std::string error;
-		switch (errorCode)
-		{
-		case GL_INVALID_ENUM:
-			error = "INVALID_ENUM";
-			break;
-		case GL_INVALID_VALUE:
-			error = "INVALID_VALUE";
-			break;
-		case GL_INVALID_OPERATION:
-			error = "INVALID_OPERATION";
-			break;
-		case GL_STACK_OVERFLOW:
-			error = "STACK_OVERFLOW";
-			break;
-		case GL_STACK_UNDERFLOW:
-			error = "STACK_UNDERFLOW";
-			break;
-		case GL_OUT_OF_MEMORY:
-			error = "OUT_OF_MEMORY";
-			break;
-		case GL_INVALID_FRAMEBUFFER_OPERATION:
-			error = "INVALID_FRAMEBUFFER_OPERATION";
-			break;
-		}
-        el::Loggers::getLogger("logger")->info("%s|%s|%s", error.c_str(), file, line);
-	}
-	return errorCode;
-}
-
 namespace GameEngine
 {
     extern AssetManager *g_pAssetManager;
@@ -107,7 +72,6 @@ namespace GameEngine
 
     void GraphicsManager::PrepareMaterial(RendererCammand rC)
     {
-        glCheckError1();
         auto material = rC.material;
         int textureID = 0;
         auto shader = g_pShaderManager->GetShaderProgram(material->shaderID);
@@ -198,7 +162,6 @@ namespace GameEngine
                 break;
             }
         }
-        glCheckError1();
     }
 
     void GraphicsManager::BindTexture(SharedTexture texture)
